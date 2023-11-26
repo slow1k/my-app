66f4a9dba5c5bb9ee4a057700411ac3e6cffca79
slow1k <136219658+slow1k@users.noreply.github.com>
2023-11-26 13:44:32 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,62 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+
+        <div class="logo">
+          <img src="twitt.png" alt="" />
+        </div>
+        <div class="text">
+          <a href="#">home</a>
+          <a href="#">contact</a>
+          <a href="#">chat</a>
+          <a href="#">friends</a>
+          <a href="#">profile</a>
+        </div>
       </header>
+      <main>
+
+<div class="prof">
+ <img src="rectangle 3.png" alt=""/>   
+ <p>user name</p>
+</div>
+<div class="prof2">
+    <img src="rectangle 4.png" alt=""/>
+</div>
+<div class="post">
+    <img src="rectangle 2.png" alt=""/>
+</div>
+<div class="devise">
+    <img src="Group 1.png" alt=""/>
+</div>
+
+<div class="devise2">
+    <img src="rectangle 8.png" alt=""/>
+</div>
+
+
+<div class="text">
+
+<div class="text1">
+<p>12534 likes</p>
+</div>
+<div class="text2">
+<p>spider-man #dog</p>
+</div>
+<div class="text3">
+<p>view all 467 comments</p>
+</div>
+<div class="text4">
+<p>4 days ago</p>
+</div>
+
+</div>
+</main>
+
+
+
+
+
     </div>
   );
 }
