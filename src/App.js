561e7599409980c273a004d542c8cd19b4eafff5
slow1k <136219658+slow1k@users.noreply.github.com>
2024-01-07 12:58:18 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <header>
-
-        <div class="logo">
-          <img src="twitt.png" alt="" />
-        </div>
-        <div class="text">
-          <a href="#">main</a>
-          <a href="#">contact</a>
-          <a href="#">chat</a>
-          <a href="#">friend</a>
-          <a href="#">profile</a>
-        </div>
-      </header>
+     <Header />
       <main>
 
 <div class="prof">
---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    
+       <header>
+
+<div class="logo">
+  <img src="twitt.png" alt="" />
+</div>
+<div class="text">
+  <a href="#">main</a>
+  <a href="#">contact</a>
+  <a href="#">chat</a>
+  <a href="#">friend</a>
+  <a href="#">profile</a>
+</div>
+</header>
+    
+  )
+}
